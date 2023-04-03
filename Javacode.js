427abd7251bbee0f0da5e6bec6dbd323bf6c45bd
Maksura Nuha <94732532+Nuha24@users.noreply.github.com>
2023-04-03 22:52:49 +0600

    Add files via upload

---
+++ Javacode.js
@@ -0,0 +1,245 @@
+var data= {
+    chatinit:{
+        title: ["Hey <span class='emoji'> &#128075;</span>","I'm TroiBot","You need any help?"],
+        options: ["Chat <span class='emoji'>💬</span>","Movies <span class='emoji'> 📽️ </span>","News <span class='emoji'>📰</span>","Shopping <span class='emoji'> 🛒</span>","Song <span class='emoji'> 🎶</span>"]
+    },
+
+    chat:{
+        title:["Hi, Chat Option isn't build yet! I'm working on it..."],
+    },
+
+    movies: {
+        title:["Thanks for your response, Please Select Category"],
+        options:['Hollywood','Bollywood','Web Series','Others'],
+        url : {
+            
+        }
+    },
+    news: {
+        title:["Thanks for your response, Please Select Category"],
+        options:["The Daily Star","CNN","BBC","Prothom Alo"],
+        url : {
+            more:"https://www.thedailystar.net/",
+            link:["https://edition.cnn.com/","https://www.bbc.com/", "https://www.prothomalo.com/"]
+        }
+    },
+    shopping: {
+        title:["Thanks for your response, Please Select Category"],
+        options:['Electronics','Beauty products','Mobiles','Men Fashion','Women fashion'],
+        url : {
+            
+        }
+    },
+    electronics: {
+        title:["Thanks for your response, Please Select Category"],
+        options:['Televisions','Cameras','Gaming Consoles','Headphones','Speakers'],
+        url : {
+            more:"https://www.alibaba.com/Consumer-Electronics_p44?src=sem_ggl&field=UG&from=sem_ggl&cmpgn=9774693808&adgrp=102667200911&fditm=&tgt=kwd-296668616325&locintrst=9070153&locphyscl=9076888&mtchtyp=b&ntwrk=g&device=c&dvcmdl=&creative=637261462740&plcmnt=&plcmntcat=&aceid=&position=&gclid=Cj0KCQjwz6ShBhCMARIsAH9A0qWbTU7ya-6jPUPhnpZvoI0qaKOF1aIbjdx_PQsgfKHqNdlewMJgAKUaApMtEALw_wcB",
+            link:["https://www.alibaba.com/Consumer-Electronics_p44?src=sem_ggl&field=UG&from=sem_ggl&cmpgn=9774693808&adgrp=102667200911&fditm=&tgt=kwd-296668616325&locintrst=9070153&locphyscl=9076888&mtchtyp=b&ntwrk=g&device=c&dvcmdl=&creative=637261462740&plcmnt=&plcmntcat=&aceid=&position=&gclid=Cj0KCQjwz6ShBhCMARIsAH9A0qWbTU7ya-6jPUPhnpZvoI0qaKOF1aIbjdx_PQsgfKHqNdlewMJgAKUaApMtEALw_wcB","https://www.alibaba.com/Consumer-Electronics_p44?src=sem_ggl&field=UG&from=sem_ggl&cmpgn=9774693808&adgrp=102667200911&fditm=&tgt=kwd-296668616325&locintrst=9070153&locphyscl=9076888&mtchtyp=b&ntwrk=g&device=c&dvcmdl=&creative=637261462740&plcmnt=&plcmntcat=&aceid=&position=&gclid=Cj0KCQjwz6ShBhCMARIsAH9A0qWbTU7ya-6jPUPhnpZvoI0qaKOF1aIbjdx_PQsgfKHqNdlewMJgAKUaApMtEALw_wcB","https://www.alibaba.com/Consumer-Electronics_p44?src=sem_ggl&field=UG&from=sem_ggl&cmpgn=9774693808&adgrp=102667200911&fditm=&tgt=kwd-296668616325&locintrst=9070153&locphyscl=9076888&mtchtyp=b&ntwrk=g&device=c&dvcmdl=&creative=637261462740&plcmnt=&plcmntcat=&aceid=&position=&gclid=Cj0KCQjwz6ShBhCMARIsAH9A0qWbTU7ya-6jPUPhnpZvoI0qaKOF1aIbjdx_PQsgfKHqNdlewMJgAKUaApMtEALw_wcB","https://www.alibaba.com/Consumer-Electronics_p44?src=sem_ggl&field=UG&from=sem_ggl&cmpgn=9774693808&adgrp=102667200911&fditm=&tgt=kwd-296668616325&locintrst=9070153&locphyscl=9076888&mtchtyp=b&ntwrk=g&device=c&dvcmdl=&creative=637261462740&plcmnt=&plcmntcat=&aceid=&position=&gclid=Cj0KCQjwz6ShBhCMARIsAH9A0qWbTU7ya-6jPUPhnpZvoI0qaKOF1aIbjdx_PQsgfKHqNdlewMJgAKUaApMtEALw_wcB","https://www.alibaba.com/Consumer-Electronics_p44?src=sem_ggl&field=UG&from=sem_ggl&cmpgn=9774693808&adgrp=102667200911&fditm=&tgt=kwd-296668616325&locintrst=9070153&locphyscl=9076888&mtchtyp=b&ntwrk=g&device=c&dvcmdl=&creative=637261462740&plcmnt=&plcmntcat=&aceid=&position=&gclid=Cj0KCQjwz6ShBhCMARIsAH9A0qWbTU7ya-6jPUPhnpZvoI0qaKOF1aIbjdx_PQsgfKHqNdlewMJgAKUaApMtEALw_wcB"]
+        }
+    },
+    beauty: {
+        title:["Thanks for your response, Please Select Category"],
+        options:['Makeup','Nails','Skin Care','Fragrance','Hair care'],
+        url : {
+            more:"https://www.youtube.com/@webhubhttps://hudabeauty.com/us/en_US/home/videos",
+            link:["https://www.sheglam.com/","https://fentybeauty.com/","https://www.rarebeauty.com/","https://kyliecosmetics.com/"]
+        }
+    },
+    mobiles: {
+        title:["Thanks for your response, Please Select Category"],
+        options:['Mobile Phones','Cases & Covers','Power Banks','Tablets'],
+        url : {
+            more:"https://www.daraz.com.bd/",
+            link:["https://www.daraz.com.bd/","https://www.daraz.com.bd/","https://www.daraz.com.bd/"]
+        }
+    },
+    men: {
+        title:["Thanks for your response","These are some results based on your input","Click on it to know more"],
+        options:['Clothing','Shirts','T-shirts','Innerwear','Jeans'],
+        url : {
+            more:"https://www.amazon.com/gp/goldbox?ref_=nav_cs_gb&deals-widget=%257B%2522version%2522%253A1%252C%2522viewIndex%2522%253A0%252C%2522presetId%2522%253A%252233FBCE1DFDB94C58F71E39810D428C43%2522%252C%2522departments%2522%253A%255B%25222102313011%2522%252C%25221040658%2522%255D%252C%2522sorting%2522%253A%2522FEATURED%2522%257D",
+            link:["#","#","#","#","#"]
+        }
+    },
+    women: {
+        title:["Thanks for your response","These are some results based on your input","Click on it to know more"],
+        options:['Clothing','Western Wear','Ethnic Wear','Top Brands'],
+        url : {
+            more:"https://gucci.cpm/us/en/ca/women-c-women",
+            link:["#","#","#","#"]
+        }
+    },
+    song: {
+        title:["Thanks for your response, Please Select Category<span class='emoji'> &#127925;</span>"],
+        options: ["Bangla","Hindi","English","Trending","Olds"],
+        url : {
+            more:"https://open.spotify.com/artist/6xArchxfpOsC8rvclgSeAl?si=Uqonbr9aS26Y3MWnOXc1qA",
+            link:["https://open.spotify.com/artist/4gdMJYnopf2nEUcanAwstx?si=R5k5j6gcS3K-1dlZlQBsfA","https://open.spotify.com/playlist/37i9dQZF1DX4WYpdgoIcn6?si=d932d9fc0e7c465a","https://open.spotify.com/playlist/37i9dQZF1DWVlLVXKTOAYa?si=7f259b39276d4b99","https://open.spotify.com/playlist/3HFb592I7MuJmL1yryzEP6?si=7d7e472a4fb84e43"]
+        }
+    },
+    hollywood: {
+        title: ["Thanks for your response","Please Select Genre"],
+        options: ["Comedy","Horror","Sci-Fi","Action","Drama"],
+        url: {
+            more:"https://ww1.yesmoviesfree.live/",
+            link:["https://ww1.yesmoviesfree.live/movies","https://bflix.gg/genre/horror","https://ww1.yesmoviesfree.live/watch-the-batman-2022-online-free","https://ww1.yesmoviesfree.live/watch-interstellar-2014-online-free"]
+        }
+    },
+    bollywood: {
+        title: ["Thanks for your response","Please Select Genre"],
+        options: ["Comedy","Sci-Fi","Action","Romance"],
+        url: {
+            more:"https://www.imdb.com/india/top-rated-indian-movies/",
+            link:["https://bflix.gg/movie/watch-phir-hera-pheri-65064","https://bflix.gg/movie/watch-3-idiots-69690","https://bflix.gg/movie/watch-zindagi-na-milegi-dobara-10864","https://bflix.gg/movie/watch-sita-ramam-88327"]
+        }
+    },
+    web: {
+        title: ["Thanks for your response","Please Select Genre"],
+        options: ["Comedy","Horror","Sci-Fi","Romance","Action"],
+        url: {
+            more:"https://bflix.gg/tv-show",
+            link:["https://bflix.gg/search/friends","https://bflix.gg/genre/horror","https://bflix.gg/tv/watch-the-big-bang-theory-39508","https://bflix.gg/search/how-i-met-your-mother","https://bflix.gg/search/game-of-thrones"]
+        }
+    },
+    others: {
+        title: ["Oh! Couldn't choose any? Here's Some More Options...."],
+        options: ["YouTube","Netflix","Giphy","Hulu","Anime"],
+        url: {
+            more:"https://www.youtube.com/",
+            link:["https://www.netflix.com/bd/","https://giphy.com/","https://www.hulu.com/welcome?orig_referrer=https%3A%2F%2Fwww.google.com%2F","https://zoro.to/"]
+        }
+    },
+}
+document.getElementById("init").addEventListener("click",showChatBot);
+var cbot= document.getElementById("chat-box");
+
+var len1= data.chatinit.title.length;
+
+function showChatBot(){
+    console.log(this.innerText);
+    if(this.innerText=='START CHAT'){
+        document.getElementById('test').style.display='block';
+        document.getElementById('init').innerText='CLOSE CHAT';
+        initChat();
+    }
+    else{
+        location.reload();
+    }
+}
+
+function initChat(){
+    j=0;
+    cbot.innerHTML='';
+    for(var i=0;i<len1;i++){
+        setTimeout(handleChat,(i*500));
+    }
+    setTimeout(function(){
+        showOptions(data.chatinit.options)
+    },((len1+1)*500))
+}
+
+var j=0;
+function handleChat(){
+    console.log(j);
+    var elm= document.createElement("p");
+    elm.innerHTML= data.chatinit.title[j];
+    elm.setAttribute("class","msg");
+    cbot.appendChild(elm);
+    j++;
+    handleScroll();
+}
+
+function showOptions(options){
+    for(var i=0;i<options.length;i++){
+        var opt= document.createElement("span");
+        var inp= '<div>'+options[i]+'</div>';
+        opt.innerHTML=inp;
+        opt.setAttribute("class","opt");
+        opt.addEventListener("click", handleOpt);
+        cbot.appendChild(opt);
+        handleScroll();
+    }
+}
+
+function handleOpt(){
+    console.log(this);
+    var str= this.innerText;
+    var textArr= str.split(" ");
+    var findText= textArr[0];
+    
+    document.querySelectorAll(".opt").forEach(el=>{
+        el.remove();
+    })
+    var elm= document.createElement("p");
+    elm.setAttribute("class","test");
+    var sp= '<span class="rep">'+this.innerText+'</span>';
+    elm.innerHTML= sp;
+    cbot.appendChild(elm);
+
+    console.log(findText.toLowerCase());
+    var tempObj= data[findText.toLowerCase()];
+    handleResults(tempObj.title,tempObj.options,tempObj.url);
+}
+
+function handleDelay(title){
+    var elm= document.createElement("p");
+        elm.innerHTML= title;
+        elm.setAttribute("class","msg");
+        cbot.appendChild(elm);
+}
+
+
+function handleResults(title,options,url){
+    for(let i=0;i<title.length;i++){
+        setTimeout(function(){
+            handleDelay(title[i]);
+        },i*500)
+        
+    }
+
+    const isObjectEmpty= (url)=>{
+        return JSON.stringify(url)=== "{}";
+    }
+
+    if(isObjectEmpty(url)==true){
+        console.log("having more options");
+        setTimeout(function(){
+            showOptions(options);
+        },title.length*500)
+        
+    }
+    else{
+        console.log("end result");
+        setTimeout(function(){
+            handleOptions(options,url);
+        },title.length*500)
+        
+    }
+}
+function handleOptions(options,url){
+    for(var i=0;i<options.length;i++){
+        var opt= document.createElement("span");
+        var inp= '<a class="m-link" href="'+url.link[i]+'">'+options[i]+'</a>';
+        opt.innerHTML=inp;
+        opt.setAttribute("class","opt");
+        cbot.appendChild(opt);
+    }
+    var opt= document.createElement("span");
+    var inp= '<a class="m-link" href="'+url.more+'">'+'See more</a>';
+
+    const isObjectEmpty= (url)=>{
+        return JSON.stringify(url)=== "{}";
+    }
+
+    console.log(isObjectEmpty(url));
+    console.log(url);
+    opt.innerHTML=inp;
+    opt.setAttribute("class","opt link");
+    cbot.appendChild(opt);
+    handleScroll();
+}
+function handleScroll(){
+    var elem= document.getElementById('chat-box');
+    elem.scrollTop= elem.scrollHeight;
+}
